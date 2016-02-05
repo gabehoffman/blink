915ff83c8f669e11d4923d67501e4d2c23d69f8d
Gabe Hoffman <gabe@gfc.tv>
2016-02-05 17:28:19 -0500

    Initial question/answer demo

---
+++ Blink/BlinkModel.swift
@@ -0,0 +1,103 @@
+//
+//  BlinkModel.swift
+//  BlinkModel
+//
+//  Created by Gabe Hoffman on 2/5/16.
+//  Copyright © 2016 Hoffman Tools. All rights reserved.
+//
+
+import UIKit
+
+class BlinkModel {
+    
+    var questions: [String] = []
+    var answers: [Int] = []
+    let xAxis: String
+    let yAxis: String
+    var currentQuestionIndex = 0
+    var currentQuestion: String {
+        return questions[currentQuestionIndex]
+    }
+    var currentAnswer: Int {
+        return answers[currentQuestionIndex]
+    }
+    var nextQuestion: String {
+        return questions[self.nextIndex()]
+    }
+    var nextAnswer: Int {
+        return answers[self.nextIndex()]
+    }
+    var previousQuestion: String {
+        return questions[self.previousIndex()]
+    }
+    var previousAnswer: Int {
+        return answers[self.previousIndex()]
+    }
+    
+    init() {
+        xAxis = "Group"
+        yAxis = "Individual"
+        
+        for i in 1...6 {
+            questions.append("How do you rate yourself in regard to \(xAxis) Question \(i)")
+            answers.append(5)
+            
+            questions.append("How do you rate yourself in regard to \(yAxis) Question \(i)")
+            answers.append(5)
+        }
+    
+    }
+    
+    func nextIndex() -> Int {
+        // Go to the next question or return to the beginning
+        var questionIndex = currentQuestionIndex + 1
+        if questionIndex == questions.count {
+            // Loop back to the begining
+            questionIndex = 0
+            return questionIndex
+        } else {
+            // return next question
+            return questionIndex
+        }
+    }
+    
+    func previousIndex() -> Int {
+        // Go to the next question or return to the beginning
+        var questionIndex = currentQuestionIndex - 1
+        if questionIndex < 0 {
+            // Loop back to the end
+            questionIndex = questions.count - 1
+            return questionIndex
+        } else {
+            // return previous question
+            return questionIndex
+        }
+        
+    }
+    
+    func next() -> Int {
+        // Go to the next question or return to the beginning
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            // Loop back to the begining
+            currentQuestionIndex = 0
+            return currentQuestionIndex
+        } else {
+            // return next question
+            return currentQuestionIndex
+        }
+    }
+    
+    func previous() -> Int {
+        // Go to the next question or return to the beginning
+        currentQuestionIndex -= 1
+        if currentQuestionIndex < 0 {
+            // Loop back to the end
+            currentQuestionIndex = questions.count - 1
+            return currentQuestionIndex
+        } else {
+            // return previous question
+            return currentQuestionIndex
+        }
+    }
+}
--- Blink/ViewController.swift
+++ Blink/ViewController.swift
@@ -9,17 +9,107 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let blink = BlinkModel()
+    
+    @IBOutlet var currentQuestionLabel: UILabel!
+    @IBOutlet var currentQuestionLabelCenterXConstraint: NSLayoutConstraint!
+    
+    @IBOutlet var nextQuestionLabel: UILabel!
+    @IBOutlet var nextQuestionLabelCenterXConstraint: NSLayoutConstraint!
+    
+    @IBOutlet var answerLabel: UILabel!
+    @IBOutlet var answerSlider: UISlider!
+    
+    
+    @IBAction func showNextQuestion(sender: AnyObject) {
+        // Save Current Values
+        blink.answers[blink.currentQuestionIndex] = Int(answerSlider.value)
+        
+        // Set next question/answer values
+        nextQuestionLabel.text = blink.nextQuestion
+        answerLabel.text = "\(blink.nextAnswer)"
+        let nextValue: Float = Float(blink.nextAnswer)
+        answerSlider.setValue(nextValue, animated: true)
+        
+        // Go to next question/answer
+        blink.next()
+        animateLabelTransitions()
+    }
+    
+    @IBAction func swipeNextQuestion(sender: AnyObject) {
+        showNextQuestion(sender)
+    }
+    
+    @IBAction func swipePreviousQuestion(sender: AnyObject) {
+        // Save Current Value
+        blink.answers[blink.currentQuestionIndex] = Int(answerSlider.value)
+        
+        // Set previous question/answer values
+        nextQuestionLabel.text = blink.previousQuestion
+        answerLabel.text = "\(blink.previousAnswer)"
+        let previousValue: Float = Float(blink.previousAnswer)
+        answerSlider.setValue(previousValue, animated: true)
+        
+        // Go to previous question/answer
+        blink.previous()
+        animateLabelTransitions()
+    }
+    
+    @IBAction func sliderValueChanged(sender: UISlider) {
+        answerLabel.text = "\(Int(answerSlider.value))"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        currentQuestionLabel.text = blink.questions[0]
+        answerLabel.text = "\(blink.currentAnswer)"
+        updateOffScreenLabel()
     }
-
+    
+    func updateOffScreenLabel() {
+        let screenwidth = view.frame.width
+        nextQuestionLabelCenterXConstraint.constant = -screenwidth
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Set the label's initial alpha
+        nextQuestionLabel.alpha = 0
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func animateLabelTransitions() {
+        view.layoutIfNeeded()
+        
+        // Animate the alpha and the center X contraints
+        let screenWidth = view.frame.width
+        self.nextQuestionLabelCenterXConstraint.constant = 0
+        self.currentQuestionLabelCenterXConstraint.constant += screenWidth
+        
+        UIView.animateWithDuration(0.5,
+            delay: 0,
+            usingSpringWithDamping: 0.6,
+            initialSpringVelocity: 0.5,
+            options: [.CurveLinear],
+            animations: {
+                self.currentQuestionLabel.alpha = 0
+                self.nextQuestionLabel.alpha = 1
+                
+                self.view.layoutIfNeeded()
+            },
+            completion: { _ in
+                swap(&self.currentQuestionLabel, &self.nextQuestionLabel)
+                swap(&self.currentQuestionLabelCenterXConstraint, &self.nextQuestionLabelCenterXConstraint)
+                
+                self.updateOffScreenLabel()
+        })
+    }
 }
 
