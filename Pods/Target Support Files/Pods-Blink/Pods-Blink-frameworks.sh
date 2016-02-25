cf2ce0a5a3b70e8fec683db7958746e54f3e8f53
Gabe at Work <gabeatwork@Gabes-Mac-Pro.local>
2016-02-25 16:31:24 -0500

    add ios charts podfile

--- Pods/Target Support Files/Pods-Blink/Pods-Blink-frameworks.sh
+++ Pods/Target Support Files/Pods-Blink/Pods-Blink-frameworks.sh
@@ -84,8 +84,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods-Blink/Charts.framework"
   install_framework "Pods-Blink/Freddy.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods-Blink/Charts.framework"
   install_framework "Pods-Blink/Freddy.framework"
 fi
