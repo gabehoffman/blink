042c8032522efea84d9c6ee6f819eb6d0840d43d
Gabe Hoffman <gabehoffman@me.com>
2016-03-03 10:59:17 -0500

    Add 100M icons

---
+++ Blink/WelcomeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  WelcomeViewController.swift
+//  Blink
+//
+//  Created by Gabe at Work on 3/2/16.
+//  Copyright © 2016 Hoffman Tools. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+}
